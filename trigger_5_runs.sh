9ae2a121d1fb1297dd7a4864dc21a26b759adcfb
lorenz369 <lorenz369@outlook.de>
2025-01-24 17:46:12 +0100

    update script

--- trigger_5_runs.sh
+++ trigger_5_runs.sh
@@ -2,9 +2,9 @@
 
 # Run main_Retrieval_Retro.py with 3 different difficulties using nohup for background processes
 mkdir -p logs
-nohup python main_Retrieval_Retro.py --seed 42 --difficulty easy --checkpoint_path checkpoints/RR/easy/epoch100_top5_acc_0.6762_42.pt >> logs/run_easy_seed42.log 2>&1 &
-nohup python main_Retrieval_Retro.py --seed 42 --difficulty medium --checkpoint_path checkpoints/RR/medium/epoch250_top5_acc_0.4391_42.pt>> logs/run_medium_seed42.log 2>&1 &
-nohup python main_Retrieval_Retro.py --seed 42 --difficulty hard --checkpoint_path checkpoints/RR/hard/epoch450_top5_acc_0.4008_42.pt >> logs/run_hard_seed42.log 2>&1 &
+nohup python main_Retrieval_Retro.py --seed 42 --difficulty easy --checkpoint_path checkpoints/RR/easy/epoch100_top5_acc_0.6762_42.pt > logs/run_easy_seed42.log 2>&1 &
+nohup python main_Retrieval_Retro.py --seed 42 --difficulty medium --checkpoint_path checkpoints/RR/medium/epoch250_top5_acc_0.4391_42.pt > logs/run_medium_seed42.log 2>&1 &
+nohup python main_Retrieval_Retro.py --seed 42 --difficulty hard --checkpoint_path checkpoints/RR/hard/epoch450_top5_acc_0.4008_42.pt > logs/run_hard_seed42.log 2>&1 &
 
 nohup python main_Retrieval_Retro.py --seed 69 --difficulty easy > logs/run_easy_seed69.log 2>&1 &
 nohup python main_Retrieval_Retro.py --seed 69 --difficulty medium > logs/run_medium_seed69.log 2>&1 &
