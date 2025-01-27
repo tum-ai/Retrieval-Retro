6996b3fe642a5a7abc5a3e4f41fdb77a9a4e6ac0
lorenz369 <lorenz369@outlook.de>
2025-01-27 14:06:21 +0100

    Update eval

--- evaluation/RR/evaluate_all.sh
+++ evaluation/RR/evaluate_all.sh
@@ -3,24 +3,24 @@
 # Easy difficulty
 # Easy difficulty
 for seed in 69 777 888; do
-    python eval_3.py --input /home/thorben/code/mit/Retrieval-Retro/results/easy/RR_easy_best_results_${seed}_updated.json \
-                     --output /home/thorben/code/mit/Retrieval-Retro/results/easy/RR_easy_best_results_${seed}_eval.json \
-                     --log /home/thorben/code/mit/Retrieval-Retro/results/easy/RR_easy_best_results_${seed}_eval.log \
-                     | tee -a /home/thorben/code/mit/Retrieval-Retro/results/easy/RR_easy_best_results_${seed}_scores.txt
+    python eval_3.py --input /home/thorben/code/mit/Retrieval-Retro/results/easy/RR_easy_best_results_${seed}_naive_updated.json \
+                     --output /home/thorben/code/mit/Retrieval-Retro/results/easy/RR_easy_best_results_${seed}_naive_eval.json \
+                     --log /home/thorben/code/mit/Retrieval-Retro/results/easy/RR_easy_best_results_${seed}_naive_eval.log \
+                     | tee -a /home/thorben/code/mit/Retrieval-Retro/results/easy/RR_easy_best_results_${seed}_naive_scores.txt
 done
 
 # Medium difficulty 
 for seed in 69 777 888; do
-    python eval_3.py --input /home/thorben/code/mit/Retrieval-Retro/results/medium/RR_medium_best_results_${seed}_updated.json \
-                     --output /home/thorben/code/mit/Retrieval-Retro/results/medium/RR_medium_best_results_${seed}_eval.json \
-                     --log /home/thorben/code/mit/Retrieval-Retro/results/medium/RR_medium_best_results_${seed}_eval.log \
-                     | tee -a /home/thorben/code/mit/Retrieval-Retro/results/medium/RR_medium_best_results_${seed}_scores.txt
+    python eval_3.py --input /home/thorben/code/mit/Retrieval-Retro/results/medium/RR_medium_best_results_${seed}_naive_updated.json \
+                     --output /home/thorben/code/mit/Retrieval-Retro/results/medium/RR_medium_best_results_${seed}_naive_eval.json \
+                     --log /home/thorben/code/mit/Retrieval-Retro/results/medium/RR_medium_best_results_${seed}_naive_eval.log \
+                     | tee -a /home/thorben/code/mit/Retrieval-Retro/results/medium/RR_medium_best_results_${seed}_naive_scores.txt
 done
 
 # Hard difficulty
 for seed in 69 777 888; do
-    python eval_3.py --input /home/thorben/code/mit/Retrieval-Retro/results/hard/RR_hard_best_results_${seed}_updated.json \
-                     --output /home/thorben/code/mit/Retrieval-Retro/results/hard/RR_hard_best_results_${seed}_eval.json \
-                     --log /home/thorben/code/mit/Retrieval-Retro/results/hard/RR_hard_best_results_${seed}_eval.log \
-                     | tee -a /home/thorben/code/mit/Retrieval-Retro/results/hard/RR_hard_best_results_${seed}_scores.txt
+    python eval_3.py --input /home/thorben/code/mit/Retrieval-Retro/results/hard/RR_hard_best_results_${seed}_naive_updated.json \
+                     --output /home/thorben/code/mit/Retrieval-Retro/results/hard/RR_hard_best_results_${seed}_naive_eval.json \
+                     --log /home/thorben/code/mit/Retrieval-Retro/results/hard/RR_hard_best_results_${seed}_naive_eval.log \
+                     | tee -a /home/thorben/code/mit/Retrieval-Retro/results/hard/RR_hard_best_results_${seed}_naive_scores.txt
 done
